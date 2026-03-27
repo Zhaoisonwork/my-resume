1c7a828590122e1b1f3ad6ba933ee1a5a6f79a11
Iamnotsnowwhite <118403314+Iamnotsnowwhite@users.noreply.github.com>
2026-03-27 15:04:57 +0100

    简化完成

---
+++ .history/src/pages/AboutPage_20260327150244.tsx
@@ -0,0 +1,22 @@
+// src/pages/AboutPage.tsx
+
+import React from 'react';
+// 假设 AboutPage.tsx 在 src/pages 目录下
+import About from '../components/About';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <main>
+        <h1><menu></menu></h1>
+        <h2>
+        <p>This is the about page <br />
+          where you can learn more about me.</p>
+        </h2>
+      </main>
+      <About/>
+    </div>
+  );
+}
+
+export default AboutPage;
